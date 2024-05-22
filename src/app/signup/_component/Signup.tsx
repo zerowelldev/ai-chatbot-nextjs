b0d9a150ae94ce7c8084702556d94060542d04b9
zerowelldev <ai_team@zerowell.co.kr>
2024-05-22 15:43:02 +0900

    회원가입 페이지 html css 작성 완료

---
+++ src/app/signup/_component/Signup.tsx
@@ -0,0 +1,32 @@
+import BackButton from '@/app/_component/BackButton';
+import style from './signup.module.css';
+
+export default function Signup() {
+  return (
+    <div className={style.loginContainer}>
+      <div className={style.loginForm}>
+        <div className={style.title}>
+          <BackButton />
+          <p>회원가입</p>
+        </div>
+        <div className={style.idDiv}>
+          <p>아이디</p>
+          <input placeholder='아이디를 입력해주세요' />
+        </div>
+        <div className={style.passwordDiv}>
+          <p>비밀번호</p>
+          <input placeholder='비밀번호를 입력해주세요' />
+        </div>
+        <div className={style.passwordCheckDiv}>
+          <p>비밀번호 확인</p>
+          <input placeholder='비밀번호를 다시 입력해주세요' />
+        </div>
+        <button className={style.loginButton}>회원가입</button>
+      </div>
+      <div className={style.redirectToLogin}>
+        <p>이미 계정이 있으신가요?</p>
+        <button>로그인</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import Signup from './_component/Signup';
+
+export default function SignupPage() {
+  return <Signup />;
+}
